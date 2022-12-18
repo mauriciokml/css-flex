32411938c5d5481a069c80705fa1104a41d8f887
Mauricio KML <mauriciu_u@hotmail.com>
2022-12-18 08:59:55 -0300

    scroll suave nas hashtags

--- script.js
+++ script.js
@@ -16,3 +16,22 @@ if(tabMenu.length && tabContent.length) {
     });
   })
 }
+
+
+const linksInternos = document.querySelectorAll('.js-menu a[href^="#"');
+
+function scrollToSection(event) {
+  event.preventDefault();
+  const href = event.currentTarget.getAttribute('href');
+  const section = document.querySelector(href);
+  const topo = section.offsetTop;
+  
+  window.scrollTo({
+    top: topo,
+    behavior: 'smooth',
+  });
+}
+
+linksInternos.forEach((link) => {
+  link.addEventListener('click', scrollToSection);
+})

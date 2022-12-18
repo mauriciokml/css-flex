3a7eb4b86f50e0cdddfa6d9cbe23d1df7f774820
Mauricio KML <mauriciu_u@hotmail.com>
2022-12-18 10:22:36 -0300

    att animacao scroll

--- script.js
+++ script.js
@@ -35,3 +35,21 @@ function scrollToSection(event) {
 linksInternos.forEach((link) => {
   link.addEventListener('click', scrollToSection);
 })
+
+const sections = document.querySelectorAll('.js-scroll');
+
+const windowMetade = window.innerHeight * 0.6;
+
+function animaScroll() {
+  sections.forEach((section) => {
+    const sectionTop = section.getBoundingClientRect().top - windowMetade;
+    console.log(sectionTop);
+    if(sectionTop < 0) {
+      section.classList.add('ativo');
+    }
+  })
+}
+
+animaScroll();
+
+window.addEventListener('scroll', animaScroll);
